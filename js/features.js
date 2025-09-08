ef0db59d51519e561ada5a0bd3b6b28b6d76a22c
md ismail <mdismail85773@gmail.com>
2025-09-08 23:15:25 +0800

    button bg change functionality add

--- js/features.js
+++ js/features.js
@@ -9,16 +9,39 @@ document.getElementById('donation-btn').addEventListener('click', function(){
 
 //get the history button id for add event listener  
 document.getElementById('history-btn').addEventListener('click', function(){
-    console.log('history button click');
+    // console.log('history button click');
     // call the function which is come from  utilitiz.js file and pass the id
     pageRelod('history-section');
 })
 
 
-// get the blog button id for add event listener
+// get the blog button id for add event listener 
 document.getElementById('blog-btn').addEventListener('click', function(){
-    console.log('blog button click');
+    // console.log('blog button click');
     // call the function which is come from  utilitiz.js file and pass the id
     pageRelod('blog-section');
 })
+
+
+            // button  bg color change function
+
+
+
+// this function for change button color when clicked
+document.getElementById('donation-btn').addEventListener('click', function(){
+    buttonColorChange('donation-btn');
+});
+
+
+
+// this function for change button color when clicked
+document.getElementById('history-btn').addEventListener('click', function(){
+    buttonColorChange('history-btn');
+});
+
+
+// this function for change button color when clicked
+document.getElementById('blog-btn').addEventListener('click', function(){
+    buttonColorChange('blog-btn');
+});
 
--- js/utilitiz.js
+++ js/utilitiz.js
@@ -11,9 +11,33 @@ function pageRelod(id){
 
     // show the section which id is passed in the function for clicked button
     document.getElementById(id).classList.remove('hidden');
-
-
 };
 
 
 
+function pageRelod(id){
+    // check the id
+    // console.log(id);
+
+    // hide both section first
+    document.getElementById('card-section').classList.add('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+    document.getElementById('blog-section').classList.add('hidden');
+
+
+    // show the section which id is passed in the function for clicked button
+    document.getElementById(id).classList.remove('hidden');
+};
+
+//  this function for change button color when clicked
+function buttonColorChange(id){
+    // console.log(id);
+    
+    // first remove all button bg color
+    document.getElementById('donation-btn').classList.remove('bg-green-400');
+    document.getElementById('history-btn').classList.remove('bg-green-400');
+    // document.getElementById('blog-btn').classList.remove('bg-green-400');
+
+    // then add the bg color to the clicked button using id
+    document.getElementById(id).classList.add('bg-green-400');
+};
